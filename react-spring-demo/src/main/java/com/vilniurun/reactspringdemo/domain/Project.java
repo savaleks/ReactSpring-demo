43a1217c77d3f72a6d7ca32f4c9c8c7b00190951
savaleks <savaleks@gmail.com>
2019-10-22 11:37:54 +0300

    updated Object

--- react-spring-demo/src/main/java/com/vilniurun/reactspringdemo/domain/Project.java
+++ react-spring-demo/src/main/java/com/vilniurun/reactspringdemo/domain/Project.java
@@ -1,4 +1,38 @@
 package com.vilniurun.reactspringdemo.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String athleteName;
+
+    private String athleteIdentifier;
+
+    private String description;
+
+    private Date created_At;
+
+    private Date updated_At;
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
+
 }
