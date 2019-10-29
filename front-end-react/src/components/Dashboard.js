245d90d59bb08f712e29242741a4aca9f5446448
savaleks <savaleks@gmail.com>
2019-10-29 10:58:14 +0200

    added bootstrap

--- front-end-react/src/components/Dashboard.js
+++ front-end-react/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import ProjectItem from './project/ProjectItem'
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div>
                 <h1>Welcome to the Dashboard</h1>
+                <ProjectItem />
             </div>
         )
     }
