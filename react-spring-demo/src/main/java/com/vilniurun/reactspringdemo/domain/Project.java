017fa5d2aded1676e1a1e8b172f4e098a0e3d043
savaleks <savaleks@gmail.com>
2019-11-12 19:50:13 +0200

    updated project object

--- react-spring-demo/src/main/java/com/vilniurun/reactspringdemo/domain/Project.java
+++ react-spring-demo/src/main/java/com/vilniurun/reactspringdemo/domain/Project.java
@@ -32,6 +32,7 @@ public class Project {
     private Date end_date;
 
     @JsonFormat(pattern="yyyy-MMM-dd HH:mm")
+    @Column(updatable = false)
     private Date created_At;
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date updated_At;
