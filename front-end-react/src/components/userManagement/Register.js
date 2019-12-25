8d250d9e82f0cc8b7de6a56b0b36211d487c4e73
savaleks <savaleks@gmail.com>
2019-12-25 22:35:41 +0200

    registration validation

--- front-end-react/src/components/userManagement/Register.js
+++ front-end-react/src/components/userManagement/Register.js
@@ -1,109 +1,143 @@
 import React, { Component } from "react";
-import {createNewUser } from "../../actions/securityAction";
+import { createNewUser } from "../../actions/securityAction";
 import PropTypes from "prop-types";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import classnames from "classnames";
 
 class Register extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      email:"",
-      firstName:"",
-      lastName:"",
-      password:"",
-      confirmPassword:"",
-      errors:{}
+      email: "",
+      firstName: "",
+      lastName: "",
+      password: "",
+      confirmPassword: "",
+      errors: {}
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange(e){
-    this.setState({[e.target.name]:e.target.value})
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  onSubmit(e){
+  onSubmit(e) {
     e.preventDefault();
     const newUser = {
-      email:this.state.email,
-      firstName:this.state.firstName,
-      lastName:this.state.lastName,
-      password:this.state.password,
-      confirmPassword:this.state.confirmPassword
-    }
+      email: this.state.email,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword
+    };
     this.props.createNewUser(newUser, this.props.history);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="container register-form">
         <div className="form">
           <div className="note">
             <p>This is a simpleRegister Form</p>
           </div>
-        <form onSubmit = {this.onSubmit}>
-        <div className="form-content">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="First Name *"
-                name="firstName"
-                value={this.state.firstName}
-                onChange={this.onChange}
-              />
+          <form onSubmit={this.onSubmit}>
+            <div className="form-content">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className={classnames("form-control", {
+                        "is-invalid": errors.firstName
+                      })}
+                      placeholder="First Name *"
+                      name="firstName"
+                      value={this.state.firstName}
+                      onChange={this.onChange}
+                    />
+                    {errors.firstName && (
+                      <div className="invalid-feedback">{errors.firstName}</div>
+                    )}
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className={classnames("form-control", {
+                        "is-invalid": errors.lastName
+                      })}
+                      placeholder="Last Name *"
+                      name="lastName"
+                      value={this.state.lastName}
+                      onChange={this.onChange}
+                    />
+                    {errors.lastName && (
+                      <div className="invalid-feedback">{errors.lastName}</div>
+                    )}
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className={classnames("form-control", {
+                        "is-invalid": errors.email
+                      })}
+                      placeholder="Email *"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.onChange}
+                    />
+                    {errors.email && (
+                      <div className="invalid-feedback">{errors.email}</div>
+                    )}
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className={classnames("form-control", {
+                        "is-invalid": errors.password
+                      })}
+                      placeholder="Your Password *"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.onChange}
+                    />
+                    {errors.password && (
+                      <div className="invalid-feedback">{errors.password}</div>
+                    )}
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className={classnames("form-control", {
+                        "is-invalid": errors.confirmPassword
+                      })}
+                      placeholder="Confirm Password *"
+                      name="confirmPassword"
+                      value={this.state.confirmPassword}
+                      onChange={this.onChange}
+                    />
+                    {errors.confirmPassword && (
+                      <div className="invalid-feedback">
+                        {errors.confirmPassword}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <input type="submit" className="btn btn-info btn-lg mt-4" />
             </div>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Last Name *"
-                name="lastName"
-                value={this.state.lastName}
-                onChange={this.onChange}
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Email *"
-                name="email"
-                value={this.state.email}
-                onChange={this.onChange}
-              />
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Your Password *"
-                name="password"
-                value={this.state.password}
-                onChange={this.onChange}
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Confirm Password *"
-                name="confirmPassword"
-                value={this.state.confirmPassword}
-                onChange={this.onChange}
-              />
-            </div>
-          </div>
-        </div>
-        <input type="submit" className="btn btn-info btn-lg mt-4" />
-      </div>
-        </form>
+          </form>
         </div>
       </div>
     );
@@ -113,10 +147,10 @@ class Register extends Component {
 Register.propTypes = {
   createNewUser: PropTypes.func.isRequired,
   errors: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   errors: state.errors
-})
+});
 
-export default connect (mapStateToProps, {createNewUser})(Register);
+export default connect(mapStateToProps, { createNewUser })(Register);
