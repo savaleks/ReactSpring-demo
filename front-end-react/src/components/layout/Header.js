41a03d9bdd71cb1e5ae41e7a234b8040775caa4d
savaleks <savaleks@gmail.com>
2019-12-30 07:29:11 +0200

    dynamic header

--- front-end-react/src/components/layout/Header.js
+++ front-end-react/src/components/layout/Header.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {logout} from "../../actions/securityAction";
 
-export default class Header extends Component {
+class Header extends Component {
+
+    logout() {
+        this.props.logout();
+        window.location.href = "/";
+    }
+
     render() {
+
+        const {validToken, user} = this.props.security
+
+        const userIsAuthenticated = (
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+            <li className="nav-item">
+                <Link to="/dashboard" className="nav-link">Dashboard</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/dashboard" className="nav-link">
+                <i className="fas fa-user-circle mr-1"/>{user.firstName}</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/logout" onClick={this.logout.bind(this)} className="nav-link">Logout</Link>
+            </li>
+            </div>
+        );
+
+        const userIsNotAuthenticated = (
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+            <li className="nav-item">
+                <Link to="/about" className="nav-link">About</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/register" className="nav-link">Sign In</Link>
+            </li>
+            <li className="nav-item">
+                <Link to="/login" className="nav-link">Log In</Link>
+            </li>
+            </div>
+        )
+
+        let headerLinks;
+
+        if (validToken && user) {
+            headerLinks = userIsAuthenticated;
+        } else {
+            headerLinks = userIsNotAuthenticated;
+        }
+
         return (
             <nav className="navbar navbar-expand-lg navbar-dark mx-background-top-linear">
                 <div className="container">
@@ -21,26 +70,7 @@ export default class Header extends Component {
                         <span className="sr-only">(current)</span>
                         </Link>
                     </li>
-
-                    <li className="nav-item">
-                        <Link to="/dashboard" className="nav-link">Dashboard</Link>
-                    </li>
-
-                    <li className="nav-item">
-                        <Link to="/about" className="nav-link">About</Link>
-                    </li>
-
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Projects</a>
-                    </li>
-
-                    <li className="nav-item">
-                        <Link to="/register" className="nav-link">Sign In</Link>
-                    </li>
-
-                    <li className="nav-item">
-                    <Link to="/login" className="nav-link">Log In</Link>
-                    </li>
+                    {headerLinks}
                     </ul>
                 </div>
                 </div>
@@ -48,3 +78,14 @@ export default class Header extends Component {
         )
     }
 }
+
+Header.propTypes = {
+    logout: PropTypes.func.isRequired,
+    security: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    security: state.security
+});
+
+export default connect (mapStateToProps, {logout})(Header);
